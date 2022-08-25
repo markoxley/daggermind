4380b50974ac583135e80644e4d58f26c1a220fb
Mark Oxley <mark.oxley@programmer.net>
2022-08-25 03:01:58 +0100

    Restored main

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"log"
-
 	"github.com/markoxley/daggermind/cmd"
 )
 
 func main() {
-	if err := cmd.Run(); err != nil {
+            	if err := cmd.Run(); err != nil {
 		log.Fatalf("unhandled error: %v", err)
 	}
 	log.Print("Done")
+
+	log.Fatal()
 }
